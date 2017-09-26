f24fb9844dbf539ebe3139ed306a9bb0adf24787
hjf <hejf@litonggroup.com.cn>
2017-09-26 17:17:42 +0800

    update 1、header完成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-  	<v-header></v-header>
-  	<div class="tab">
+  	<v-header :seller="seller"></v-header>
+  	<div class="tab border-1px">
   		<div class="tab-item">
   			<router-link to="/goods">商品</router-link>
   		</div>
@@ -16,21 +16,39 @@
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
 	import header from 'components/header/header.vue'
 	
-export default {
-  name: 'app',
-  components:{
-  	'v-header':header
-  }
-}
+	const ERR_OK = 0;//定义ok的状态码
+	
+	export default {
+  	data() {
+	  	return{
+		  	seller:{}
+	  	}
+  	},
+  	created(){
+//		ajax请求
+  		this.$http.get('/api/seller').then(response => {
+    		// get body data
+    		response = response.body;
+				if(response.errno === ERR_OK){
+					this.seller = response.data;
+				}
+  		});
+  	},
+  	components:{
+  		'v-header':header
+  	}
+	}
+
 </script>
 
 <style  lang="stylus" rel="stylesheet/stylus">
+	@import "../src/common/stylus/mixin.styl"
 	.tab{
-		display: flex;width: 100%;height: 40px;line-height: 40px;
-		.tab-item{flex: 1;text-align: center;
+		display: flex;width: 100%;height: 40px;line-height: 40px;;
+		.tab-item{flex: 1;text-align: center;border-1px(rgba(7, 17, 27, 0.1));
 			& > a{display: block;font-size: 14px;color: rgb(77,85,93);
 				&.active{color: rgb(240,20,20);}
 			}
--- src/main.js
+++ src/main.js
@@ -2,13 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import App from './App'
 import goods from 'components/goods/goods';
 import ratings from 'components/ratings/ratings';
 import seller from 'components/seller/seller';
 
-Vue.use(VueRouter);
+import "common/stylus/index.styl"
 
+// 注册
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+//定义路由，每个路由器应该映射一个组件。其中component可以是通过Vue.extend()创建的组件构造器，或者只是一个组件配置对象
 const routes = [{
   path: '/',
   redirect: '/goods'
@@ -22,12 +28,12 @@ const routes = [{
   path: '/seller',
   component: seller
 }];
-
+//创建router实例,然后传routes配置
 const router = new VueRouter({
   linkActiveClass: 'active',
   routes
 });
-
+//创建和挂载根实例,通过router配置参数注入路由,从而让整个应用都有路由功能.
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
