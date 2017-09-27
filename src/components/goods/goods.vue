e78897e9fd00c6df61914552b803d1fee2a779a2
hjf <hejf@litonggroup.com.cn>
2017-09-27 12:01:34 +0800

    update 1、头部功能完成

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,5 +1,12 @@
 <template>
-	<div>我是goods 啊哈哈哈</div>
+	<div class="goods">
+		<div class="menu-wrapper">
+			菜单
+		</div>
+		<div class="foods-wrapper">
+		
+		</div>
+	</div>
 </template>
 
 <script type="text/ecmascript-6">
@@ -7,4 +14,12 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+	.goods{display: flex;position: absolute;width: 100%;top: 174px;bottom: 46px;overflow: hidden;
+		.menu-wrapper{flex: 0 0 80px;width: 80px;background-color: #f3f5f7;
+			
+		}
+		.foods-wrapper{flex: 1;
+			
+		}
+	}
 </style>
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -30,13 +30,48 @@
 		<div class="background">
 			<img :src="seller.avatar" width="100%" height="100%">
 		</div>
-		<div class="detail" v-show='detailShow'>
-			
-		</div>
+		<!--模态框-->
+		<transition name="fade">
+			<div class="detail" v-show='detailShow'>
+				<div class="detail-wrapper clearfix">
+					<div class="detail-main">
+						<h1 class="name">{{seller.name}}</h1>
+						<div class="start-wrap">
+							<star :size="48" :score="seller.score"></star>
+						</div>
+						<div class="title">
+							<div class="line"></div>
+							<div class="text">优惠信息</div>
+							<div class="line"></div>
+						</div>
+						<ul v-if="seller.supports" class="supports">
+							<li class="support-item" v-for="item in seller.supports">
+								<span class="icon" :class="classMap[item.type]"></span>
+								<span class="text">{{item.description}}</span>
+							</li>
+						</ul>
+						<div class="title">
+							<div class="line"></div>
+							<div class="text">商家公告</div>
+							<div class="line"></div>
+						</div>
+						<div class="bulletin">
+							<p class="content">{{seller.bulletin}}</p>
+						</div>
+					</div>
+				</div>
+				<div class="detail-close" @click="hideDetail">
+					<i class="icon-close"></i>
+				</div>
+			</div>
+		</transition>
+		
 	</div>
 </template>
 
 <script type="text/ecmascript-6">
+	import star from "../../components/star/star"
+	
 	export default {
 		props: {
 			seller:{
@@ -52,9 +87,15 @@
 			showDetail() {
         		this.detailShow = true;
       		},
+      		hideDetail() {
+      			this.detailShow = false;
+      		}
 		},
 		created() {
 			this.classMap = ['decrease','discount','special','invoice','guarantee'];
+		},
+		components: {
+			star
 		}
 	}
 </script>
@@ -74,13 +115,12 @@
 				}
 				.description{margin-bottom: 10px;line-height: 12px;font-size: 12px;}
 				.support{
-					.icon{display: inline-block;vertical-align: top;width: 12px;height: 12px;margin-right: 4px;background-size: 12px 12px;background-repeat: no-repeat;
-						bg-image('decrease_1')
-						/*&.decrease{bg-image('decrease_1')}*/
-            			/*&.discount{bg-image('discount_1')}
-              			&.guarantee{bg-image('guarantee_1')} 
-			            &.invoice{bg-image('invoice_1')}
-              			&.special{bg-image('special_1')}*/
+					.icon{display: inline-block;vertical-align: top;width: 12px;height: 12px;margin-right: 4px;
+						&.decrease{bg-image('decrease_1');background-size: 12px 12px;background-repeat: no-repeat;}
+						&.discount{bg-image('discount_1');background-size: 12px 12px;background-repeat: no-repeat;}
+              			&.guarantee{bg-image('guarantee_1');background-size: 12px 12px;background-repeat: no-repeat;} 
+			            &.invoice{bg-image('invoice_1');background-size: 12px 12px;background-repeat: no-repeat;}
+              			&.special{bg-image('special_1');background-size: 12px 12px;background-repeat: no-repeat;}
 					}
 					.text{line-height: 12px;font-size: 10px;}
 				}
@@ -96,6 +136,36 @@
 			.icon-keyboard_arrow_right{position: absolute;right: 12px;top: 8px;font-size: 10px;}
 		}
 		.background{position: absolute;top: 0;left: 0;width: 100%;height: 134px;z-index: -1;filter: blur(10px);}
-		.detail{position: fixed;top: 0;left: 0;z-index: 100;width: 100%;height: 100%;overflow: auto;background-color: rgba(7,17,27,0.8);}
+		.detail{position: fixed;top: 0;left: 0;z-index: 100;width: 100%;height: 100%;overflow: auto;opacity: 1;background: rgba(7, 17, 27, 0.8);backdrop-filter:blur(10px);
+			&.fade-enter-active, &.fade-leave-active{transition: all 0.5s}
+      		&.fade-enter, &.fade-leave-active{opacity: 0;background: rgba(7, 17, 27, 0)}
+			.detail-wrapper{width: 100%;min-height: 100%;
+				.detail-main{margin-top: 64px;padding-bottom: 64px;
+					.name{line-height: 16px;text-align: center;font-size: 16px;font-weight: 700;}
+					.start-wrap{margin-top: 16px;padding: 2px 0;text-align: center;}
+					.title{display: flex;width: 80%;margin: 28px auto 24px auto;
+						.line{flex: 1;position: relative;top: -6px;border-bottom: 1px solid rgba(255,255,2555,0.2);}
+						.text{padding: 0 12px;font-size: 14px;font-weight: 700;}
+					}
+					.supports{width: 80%;margin: 0 auto;
+						.support-item{padding:0  12px;margin-bottom: 12px;font-size: 0;
+							&:last-child{margin-bottom: 0;}
+							.icon{display: inline-block;width: 16px;height: 16px;vertical-align: top;margin-right: 6px;
+								&.decrease{bg-image('decrease_2');background-size: 16px 16px;background-repeat: no-repeat;}
+								&.discount{bg-image('discount_2');background-size: 16px 16px;background-repeat: no-repeat;}
+              					&.guarantee{bg-image('guarantee_2');background-size: 16px 16px;background-repeat: no-repeat;} 
+			            		&.invoice{bg-image('invoice_2');background-size: 16px 16px;background-repeat: no-repeat;}
+              					&.special{bg-image('special_2');background-size: 16px 16px;background-repeat: no-repeat;}
+							}
+							.text{line-height: 12px;font-size: 12px;}
+						}
+					}
+					.bulletin{width: 80%;margin: 0 auto;
+						.content{padding: 0 12px;line-height: 24px;font-size: 12px;font-weight: 200;}
+					}
+				}
+			}
+			.detail-close{position: relative;width: 32px;height: 32px;margin: -64px auto 0 auto;clear: both;font-size: 32px;}
+		}
 	}
 </style>
