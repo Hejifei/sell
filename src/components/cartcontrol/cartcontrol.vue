d81c62620dfd9ad03a2aae954a17a203f427d805
657414658@qq.com <2017jiayou>
2017-09-29 21:55:28 +0800

    fixed 1、修正视图不跟着变化的问题，用this.$set替代Vue.set

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,16 +1,13 @@
 <template>
 	<div class="cartcontrol">
-		<div class="cart-decrease" v-show="food.count>0">
-			<i class="icon-remove_circle_outline"></i>
-		</div>
+		<div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0"></div>
 		<div class="cart-count" v-show="food.count>0">{{food.count}}</div>
-		<div class="cart-add" @click=addCart>
-			<i class="icon-add_circle"></i>
-		</div>
+		<div class="cart-add icon-add_circle" @click.stop.prevent="addCart"></div>
 	</div>
 </template>
 
 <script type="text/ecmascript-6">
+	import Vue from 'Vue';
 	export default {
 		props: {
 			food: {
@@ -18,18 +15,22 @@
 			}
 		},
 		created() {
-			console.log(this.food)
 		},
 		methods: {
 			addCart(event) {
+				console.log("click")
 				if(!event._constructed){
 					return;
 				}
-				if(this.food.count){
-					this.food.count = 1;
+				if(!this.food.count){
+					/*this.food.count = 1;无效，因为vue中无法检测到不存在属性的变化，所以要用Vue.set*/
+					/*Vue.set只能使得视图变化一次，用this.$set可以持续变化*/
+					this.$set(this.food, 'count', 1);
 				}else{
 					this.food.count++;
 				}
+				
+				console.log(this.food.count)
 			}
 		}
 	}
@@ -37,9 +38,9 @@
 
 <style lang="stylus" rel="stylesheet/stylus">
 	.cartcontrol{font-size: 0;
-		.cart-decrease,.cart-add{display: inline-block;padding: 6px;line-height: 24px;font-size: 24px;}
-		.cart-count{display: inline-block;}
-		.cart-decrease{}
-		.cart-add{color: rgb(0,160,220);}
+		.cart-decrease,.cart-add{display: inline-block;padding: 6px;line-height: 24px;font-size: 24px;color: rgb(0,160,220);}}
+		.cart-count{display: inline-block;vertical-align: top;width: 12px;padding-top: 6px;line-height: 24px;text-align: center;font-size: 10px;color: rgb(147,153,159);}
+		.cart-decrease{count}
+		.cart-add{
 	}
 </style>
